e52944d950e14d90b0e0a8f70378e8cca74e7068
Antony Xavier <antony.xavier@iinerds.com>
2024-06-05 18:17:07 +0530

    plugins.lua file added

---
+++ lua/plugins.lua
@@ -0,0 +1,12 @@
+" ============================================================================
+" Netrw Directory Listing                                        (netrw v173)
+"   /home/antony/.config/nvim/lua
+"   Sorted by      name
+"   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,\~\=\*$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$
+"   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:special
+" ==============================================================================
+../
+./
+custom/
+kickstart/
+plugins/
