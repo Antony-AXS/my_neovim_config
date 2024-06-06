0cd4e3b75db3f88a557ecf8b9f15e98c7c6be500
Antony Xavier <antony.xavier@iinerds.com>
2024-06-06 11:35:42 +0530

    neo-tree plugin added

--- init.lua
+++ init.lua
@@ -863,6 +863,32 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+{
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    local NeoTreeGitStatus = function(opt)
+      local cmd = 'Neotree float git_status git_base=' .. opt.args
+      print(cmd)
+      vim.api.nvim_command(cmd)
+      return 0
+    end
+
+    vim.api.nvim_create_user_command('Ngs', NeoTreeGitStatus, { nargs = '?' })
+
+    ------------------------------- keymap for neo-tree -------------------------------
+    vim.keymap.set('n', '<leader>p', ':Neotree<CR>', { silent = true })
+    vim.keymap.set('n', '<leader>ng', ':Neotree float git_status<CR>', { silent = true })
+    vim.keymap.set('n', '<c-t>', ':Neotree source=filesystem reveal left toggle=true<CR>', { silent = true })
+    vim.keymap.set('n', '<leader>nb', ':Neotree float git_status git_base=' .. vim.fn.system 'git branch --show-current' .. '<CR>', {})
+    -----------------------------------------------------------------------------------
+  end,
+},
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
