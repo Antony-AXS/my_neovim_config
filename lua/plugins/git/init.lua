b29bfd0309cabe19e459e8b84747c13279e69290
Antony Xavier <antony.xavier@iinerds.com>
2024-06-11 21:51:15 +0530

    git worktree sifting notification text alteration

--- lua/plugins/git/init.lua
+++ lua/plugins/git/init.lua
@@ -133,7 +133,9 @@ return {
 
 			Worktree.on_tree_change(function(op, metadata)
 				if op == Worktree.Operations.Switch then
-					print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
+					vim.notify(
+						"Switched from " .. '"' .. metadata.prev_path .. '"' .. " to " .. '"' .. metadata.path .. '"'
+					)
 				end
 			end)
 
