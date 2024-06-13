d3ac846458f0d0e7249a71677f2bdf720c1ea241
Antony Xavier <antony.xavier@iinerds.com>
2024-06-14 01:33:49 +0530

    functions no longer be bold for solarized-osaka

--- lua/plugins/colorschemes/init.lua
+++ lua/plugins/colorschemes/init.lua
@@ -125,7 +125,7 @@ return {
 					-- Value is any valid attr-list value for `:help nvim_set_hl`
 					comments = { italic = true },
 					keywords = { italic = true },
-					functions = { bold = true },
+					functions = {},
 					variables = {},
 					-- Background styles. Can be "dark", "transparent" or "normal"
 					sidebars = "transparent", -- style for sidebars, see below
