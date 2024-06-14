1c6a3b32e7f3e7838b9a98f44946149b13f4bf8a
Antony Xavier <antony.xavier@iinerds.com>
2024-06-14 11:49:48 +0530

    event "VeryLazy"

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,8 +1,8 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	event = "VeryLazy",
 	lazy = true,
 	main = "ibl",
-	event = "VeryLazy",
 	opts = {},
 	config = function()
 		require("ibl").setup({
