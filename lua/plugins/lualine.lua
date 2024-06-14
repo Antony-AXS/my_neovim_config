4fd4c0379bf3bf0f27d5f0eda157f849238d1cde
Antony Xavier <antony.xavier@iinerds.com>
2024-06-14 11:56:58 +0530

    veryLazy event removed

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	event = "VeryLazy",
-	lazy = true,
+	-- event = "VeryLazy",
+	-- lazy = true,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local lualine = require("lualine")
