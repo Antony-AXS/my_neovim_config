93bc2b468324d84a41611f88f717498d50b23777
Antony Xavier <antony.xavier@iinerds.com>
2024-06-14 11:53:33 +0530

    extra options added

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,8 +1,14 @@
 return {
 	"stevearc/oil.nvim",
+	event = "VeryLazy",
 	config = function()
 		require("oil").setup({
 			default_file_explorer = false,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
+			win_options = {
+				wrap = true,
+			},
 		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
