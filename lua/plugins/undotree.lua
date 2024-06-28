b3b9c248f504285bbef9d2fd48cd1b04a66a8755
Antony Xavier <antony.xavier@iinerds.com>
2024-06-28 18:35:14 +0530

    undo-tree config corrections

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -16,9 +16,9 @@ return {
                                            set undofile
                                 endif
 
-                                if !exists('g:undotree_WindowLayout')
-                                    let g:undotree_WindowLayout = 1
-                                endif
+                                "if !exists('g:undotree_WindowLayout')
+                                    let g:undotree_WindowLayout = 2
+                                "endif
 
                                 let g:vim_dadbod_completion_mark = 'MYMARK'
                         ]],
@@ -32,10 +32,10 @@ return {
 		vim.keymap.set("n", "<leader>a", ":UndotreeFocus<CR>", { silent = true })
 		vim.keymap.set("n", "<leader>ut", function()
 			if undoTreeLayoutMemory == 1 then
-				vim.api.nvim_exec2([[ let g:undotree_WindowLayout = 2]], { output = false })
+				vim.api.nvim_exec2("let g:undotree_WindowLayout = 2", { output = false })
 				undoTreeLayoutMemory = 2
 			elseif undoTreeLayoutMemory == 2 then
-				vim.api.nvim_exec2([[ let g:undotree_WindowLayout = 1]], { output = false })
+				vim.api.nvim_exec2("let g:undotree_WindowLayout = 1", { output = false })
 				undoTreeLayoutMemory = 1
 			end
 		end, {})
