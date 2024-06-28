43a64876d11a2efbdc44a12e80997f7d4810d511
Antony Xavier <antony.xavier@iinerds.com>
2024-06-28 18:04:54 +0530

    undo-tree window layout switching and config changes

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,9 +1,44 @@
 return {
 	"mbbill/undotree",
 	config = function()
+		vim.api.nvim_exec2(
+			[[
+                                if has("persistent_undo")
+                                        let target_path = expand('~/.undodir')
+
+                                           " create the directory and any parent directories
+                                           " if the location does not exist.
+                                           if !isdirectory(target_path)
+                                               call mkdir(target_path, "p", 0700)
+                                           endif
+
+                                           let &undodir=target_path
+                                           set undofile
+                                endif
+
+                                if !exists('g:undotree_WindowLayout')
+                                    let g:undotree_WindowLayout = 1
+                                endif
+
+                                let g:vim_dadbod_completion_mark = 'MYMARK'
+                        ]],
+			{ output = false }
+		)
+
+		local undoTreeLayoutMemory = 2
+
 		------------------------------ keymap for undo-tree -------------------------------
 		vim.keymap.set("n", "<leader>q", ":UndotreeToggle<CR>", { silent = true })
 		vim.keymap.set("n", "<leader>a", ":UndotreeFocus<CR>", { silent = true })
+		vim.keymap.set("n", "<leader>ut", function()
+			if undoTreeLayoutMemory == 1 then
+				vim.api.nvim_exec2([[ let g:undotree_WindowLayout = 2]], { output = false })
+				undoTreeLayoutMemory = 2
+			elseif undoTreeLayoutMemory == 2 then
+				vim.api.nvim_exec2([[ let g:undotree_WindowLayout = 1]], { output = false })
+				undoTreeLayoutMemory = 1
+			end
+		end, {})
 		-----------------------------------------------------------------------------------
 	end,
 }
