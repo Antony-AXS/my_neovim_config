aa6ad76cbfff6b503e9ba86955c83d596026d4e2
Antony Xavier <antony.xavier@iinerds.com>
2024-07-20 12:44:08 +0530

    telescope version tag updated

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,7 +5,7 @@ local colorschemes = require("plugins/colorschemes")
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		-- tag = "0.1.5",
+		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	{
