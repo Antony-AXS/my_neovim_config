160d22e482f6e315ddb4409352bab694e41f3dc4
Antony Xavier <antony.xavier@iinerds.com>
2024-07-22 11:24:09 +0530

    conform config corrections

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,7 +8,12 @@ return {
 				-- Conform will run multiple formatters sequentially
 				python = { "isort", "black" },
 				-- Use a sub-list to run only the first available formatter
-				javascript = { { "prettierd", "prettier" } },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
 			},
 		})
 		-- vim.api.nvim_create_autocmd("BufWritePre", {
