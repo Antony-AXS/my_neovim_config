d52444bb13d1cbd3288258b09d28f7f76ad1fc81
Antony Xavier <antonyaxs324@gmail.com>
2024-09-02 20:08:11 +0530

    Indicator.nvim config updated

--- lua/plugins/indicator.lua
+++ lua/plugins/indicator.lua
@@ -7,23 +7,33 @@ return {
 
 		Indicator.setup({
 			indicator_event = true,
-			window_highlight_event = true,
+			window_highlight_event = false,
+			window_count_status = {
+				tab = {
+					activate = true,
+					position = { section = "x", index = 1 },
+				},
+				window = {
+					activate = true,
+					position = { section = "x", index = 1 },
+				},
+			},
 		})
 
 		vim.keymap.set("n", "<leader>bx", function()
-			Indicator.indicateCurrent(nil, nil, true)
+			Indicator.indicateCurrent()
 		end, { silent = true })
 		vim.keymap.set("n", "<leader>bv", function()
-			Indicator.indicateAll(true)
+			Indicator.indicateAll()
 		end, { silent = true })
 		vim.keymap.set("n", "<leader>bc", function()
 			Indicator.indicateAll(false)
 		end, { silent = true })
 
 		vim.keymap.set("n", "<leader>it", Indicator.indicator_event_activate, {})
-		vim.keymap.set("n", "<leader>ir", Indicator.indicator_event_diactivate, {})
+		vim.keymap.set("n", "<leader>ir", Indicator.indicator_event_deactivate, {})
 
 		vim.keymap.set("n", "<leader>iq", Indicator.window_highlight_event_activate, {})
-		vim.keymap.set("n", "<leader>iw", Indicator.window_highlight_event_diactivate, {})
+		vim.keymap.set("n", "<leader>iw", Indicator.window_highlight_event_deactivate, {})
 	end,
 }
